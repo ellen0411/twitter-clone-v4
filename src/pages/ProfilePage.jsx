e21a785b0eb4551002e46c0d16e9f376bcd8b487
ellen0411 <ellentan93@gmail.com>
2024-07-04 12:19:59 +0800

    initial commit

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,34 @@
+import { getAuth } from "firebase/auth";
+import { useContext } from "react";
+import { Container, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../components/AuthProvider";
+import ProfileSideBar from "../components/ProfileSideBar";
+import ProfileMidBody from "../components/ProfileMidBody";
+
+
+
+export default function ProfilePage() {
+
+    const auth = getAuth();
+    const navigate = useNavigate();
+    const { currentUser } = useContext(AuthContext);
+
+    if (!currentUser) {
+        navigate("/login");
+    }
+    const handleLogout = () => {
+        auth.signOut();
+    };
+
+    return (
+        <>
+            <Container>
+                <Row>
+                    <ProfileSideBar handleLogout={handleLogout} />
+                    <ProfileMidBody />
+                </Row>
+            </Container>
+        </>
+    );
+}
